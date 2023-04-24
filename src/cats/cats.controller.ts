f7d20b8a4a6b167cdd0f0bf1e9eb2751f8451ea3
Francis Maina <sebastianfrancix@gmail.com>
2023-04-24 11:46:58 +0300

    covered using @Headers, @HttpCode and @Redirect decorators in next

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,16 +1,26 @@
-import { Controller, Get, Post } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  HttpCode,
+  Header,
+  Redirect,
+} from '@nestjs/common';
 // import { Request } from 'express';
 
 @Controller('cats')
 export class CatsController {
   // Get Request
   @Get()
-  findAll(): string {
+  // @Redirect('https://mainafrancis.com', 301)
+  findAll() {
     return 'This action returns all cats';
   }
 
   // Post Request
   @Post()
+  // @HttpCode(210) //We can set custom status codes using the @HttpCode decorator at a handler level
+  @Header('Cache-Control', 'none')
   create(): string {
     return 'This action adds a new cat';
   }
