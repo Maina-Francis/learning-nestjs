c3f68d7a8328a21b6773c5de858866d242efba1c
Francis Maina <sebastianfrancix@gmail.com>
2023-04-24 16:49:41 +0300

    covered the basic overview of what is a middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
-import { Module } from '@nestjs/common';
+import { Module, NestMiddleware, MiddlewareConsumer } from '@nestjs/common';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -10,3 +11,12 @@ import { CatsModule } from './cats/cats.module';
   providers: [AppService],
 })
 export class AppModule {}
+
+// Middleware is a function which is called before the route handler. Middleware functions have access to the request and response objects and the next() middleware function in the application's request-response cycle.
+
+// Middleware functions can perform the following tasks:
+// 1.execute any code
+// 2. make changes to the request and the response objects.
+// 3. end the request-response cycle
+// 4. call the next middleware function in the stack
+// 5. if the current middleware func doesn't end the request-response cycle, it must call next() to pass control to the next middleware function. Otherwise, the request will be left hanging.
