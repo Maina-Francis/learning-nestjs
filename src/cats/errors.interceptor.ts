6f2e37e2cfa1a6ad3ab020883463734b636d36df
Francis Maina <sebastianfrancix@gmail.com>
2023-04-26 11:42:50 +0300

    wrote an errors interceptor to override thrown exceptions

---
+++ src/cats/errors.interceptor.ts
@@ -0,0 +1,21 @@
+import {
+  Injectable,
+  NestInterceptor,
+  CallHandler,
+  ExecutionContext,
+  BadGatewayException,
+} from '@nestjs/common';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+@Injectable()
+export class ErrorsInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> {
+    return next
+      .handle()
+      .pipe(catchError((err) => throwError(() => new BadGatewayException())));
+  }
+}
